53119775c7ba87ed627b31ce7262fd2fbbde6edd
Rohan6717 <rohan.techvill@gmail.com>
2023-05-12 21:50:56 +0600

    [feat] Experience update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,38 +7,41 @@ return (
   <Container>
     <Title title="Experience" />
     <div className="md:pl-24">
-      <h3>Software Engineer ( React JS)</h3>
-      <h4 className="mt-3">Company Name: Techvillage (Software  Company)   </h4>
-      <h6 className="mt-1">Address: House-19, Road-19,  Nikunja-2, Dhaka 1229</h6>
-      <h6 className="mt-1">Status:  15th July 2021 to Running.</h6>
-      <p className="mt-4">
-        Here I'm working with the front-end through React.js, APIs, User, Admin, and also working with HRM panel. Moreover, Supervised and lead a team of Front end engineers during the development of a robust upgrade version of the company's software applications, resulting in improved user experience. <br />
+      <h3>Lead Front-End Developer ( React JS)</h3>
+      <h4 className="mt-3 text-[#2c2c2c]">Company Name: Techvillage (Software  Company)   </h4>
+      <p className="mt-1 text-[#1b1b1bee] text-[16px]">Address: House-19, Road-19,  Nikunja-2, Dhaka 1229</p>
+      <p className="text-[#1b1b1bc7] text-[15px]">Status:  15th July 2021 to Running.</p>
+      <p className="mt-4 text-[17px]">
+        Here I'm working with the front-end through React.js, APIs, User, Admin, and also working with HRM panel. Moreover, Supervised and lead a team of Front end engineers during the development of a robust upgrade version of the company's software applications, resulting in improved user experience. <br /> <br />
         - Fix the bugs and optimize the overall performance. <br />
-        - Back-End Data Display <br />
-        - Website Content Analysis <br />
         - Collaborate with Back-End and Front-End Developers <br />
-        - Current Web Trends Dynamic Web Page Design & framework templates <br />
-        - Customer support ticket reply
+        - Assist with difficult code solutions and ensure proper code quality and documentation <br />
+        - Create standards such as file/folder structures and ensure that the part of the product is technically sound<br />
+        - Introduce new concepts and business logic 
+        - Mentoring developers with ensure proper git workflow, and resolve code conflict <br />
+        - Weekly tasks create for the developer, review test in progress tasks and daily reports also submit to the boss. <br />
+        - Website content analysis, current web trends dynamic web Page design & framework templates <br />
+        - Customer support ticket reply <br />
       </p>
 
       <div className="mt-11">
       <h3>React Front-End Developer</h3>
-      <h4 className="mt-3">Amader Service Global Limited (IT & Commercial Conservatory Company)</h4> 
-      <h6 className="mt-1">Address: House No: 18, Road #7, Ranavola, Uttara, Dhaka.</h6>
-      <h6 className="mt-1">Status:  7th January 2021 to 10th September 2021.</h6>
+      <h4 className="mt-3 text-[#2c2c2c]">Company: Amader Service Global Limited (IT & Commercial Conservatory Company)</h4> 
+      <p className="mt-1 text-[#1b1b1bee] text-[16px]">Address: House No: 18, Road #7, Ranavola, Uttara, Dhaka.</p>
+      <p className="text-[#1b1b1bc7] text-[15px]">Status:  7th January 2021 to 10th September 2021.</p>
 
-      <p className="mt-4">
+      <p className="mt-4 text-[17px]">
           Here I'm working with the front-end through React.js. 
           Working with API, User panel, Admin, and also working with HRM panel. I'm the team leader of the front-end team. In Addition to being a React Front-End developer, I also give ideas on system analysis and design for various sites.
       </p>
       </div>
 
       <div className="mt-11">
-      <h3>front-end developer</h3>
-      <h4 className="mt-3">HRSOFTBD (Software Development company)</h4>
-      <h6 className="mt-1">Address: 12/6, Solimullah Road, Mohammadpur, Dhaka.</h6>
-      <h6 className="mt-1">Status: 4th January 2020 to 6th January 2021.</h6>
-      <p className="mt-4">
+      <h3>Front-End developer</h3>
+      <h4 className="mt-3 text-[#2c2c2c]">Company: HRSOFTBD (Software Development company)</h4>
+      <p className="mt-1 text-[#1b1b1bee] text-[16px]">Address: 12/6, Solimullah Road, Mohammadpur, Dhaka.</p>
+      <p className="text-[#1b1b1bc7] text-[15px]">Status: 4th January 2020 to 6th January 2021.</p>
+      <p className="mt-4 text-[17px]">
           I have worked as a Front End Developer. My 
           responsibilities there were to create front-end websites, 
           project mastering, made Invoices, Email templates, level 
